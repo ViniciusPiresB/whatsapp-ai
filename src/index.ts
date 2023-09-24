0a054a848e316ba15cc946bc8f0e4f4152ea03ad
ViniPiresB <viniciuspbarreto1@gmail.com>
2023-09-24 18:24:59 -0300

    ✨ feat: Venom setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log("Hello via Bun!");
+import { create, Message } from "venom-bot";
+
+const venomClient = await create({
+  session: "test",
+  disableWelcome: true
+});
+
+venomClient.onMessage((message) => {
+  if (!message.body || message.isGroupMsg || message.from == "status@broadcast")
+    return;
+
+  console.log(message.body);
+});
