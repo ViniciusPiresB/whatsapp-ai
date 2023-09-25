d0685540a75332b905179339ee85933014e8c9b2
ViniciusPiresB <viniciuspbarreto1@gmail.com>
2023-09-24 23:23:40 -0300

    ✨ feat: Venom with gpt

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,27 @@
-import { create, Message } from "venom-bot";
+import { create } from "venom-bot";
+import ChatGPT from "./modules/chatGpt";
+import { prompt } from "./modules/chatGpt/prompts/prompt";
 
 (async () => {
   const venomClient = await create({ session: "test", disableWelcome: true });
 
-  venomClient.onMessage(message => {
-    if (!message.body || message.from == "status@broadcast") return;
+  const chatGpt = new ChatGPT(prompt);
+
+  venomClient.onMessage(async message => {
+    if (
+      !message.body ||
+      message.from == "status@broadcast" ||
+      message.isGroupMsg
+    )
+      return;
 
     console.log(message.from);
-    console.log(message.body);
+    console.log(message.content);
+
+    const responseGpt = await chatGpt.completion(message.body);
+
+    if (!responseGpt) return;
+
+    venomClient.sendText(message.from, responseGpt);
   });
 })();
